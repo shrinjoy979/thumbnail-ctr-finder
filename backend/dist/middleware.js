39824e243f045dd9494c8b276c239796d4d6825f
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-02-13 17:27:06 +0530

    add AWS f\details

---
+++ backend/dist/middleware.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authMiddleware = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const _1 = require(".");
+function authMiddleware(req, res, next) {
+    var _a;
+    const authHeader = (_a = req.headers["authorization"]) !== null && _a !== void 0 ? _a : "";
+    try {
+        const decoded = jsonwebtoken_1.default.verify(authHeader, _1.JWT_SECRET);
+        // @ts-ignore
+        if (decoded.userId) {
+            // @ts-ignore
+            req.userId = decoded.userId;
+            return next();
+        }
+        else {
+            return res.status(403).json({
+                message: "You are not logged in"
+            });
+        }
+    }
+    catch (e) {
+        return res.status(403).json({
+            message: "You are not logged in"
+        });
+    }
+}
+exports.authMiddleware = authMiddleware;
